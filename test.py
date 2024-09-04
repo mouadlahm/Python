96231a84ff4cce975ad466afd8f1b16d068ceeea
mouadlahm <mohamed.lahmam@distillersr.com>
2024-09-04 11:45:12 -0400

    test commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print(f"Hello {name}")
+    print("How are you")
+    print("mamak")
+
+def life_in_weeks(age):
+    left_age=90-age
+    left_weeks=left_age*52
+    print (f"You have {left_weeks} weeks left.")
+
+life_in_weeks(20)
